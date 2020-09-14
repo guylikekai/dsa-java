48e5064293fb2053526b9eb50a4540318ace495e
guylikekai <65786315+guylikekai@users.noreply.github.com>
2020-09-13 20:50:40 -0400

    Initial commit

---
+++ src/main/java/edu/emory/cs/algebraic/LongIntegerQuiz.java
@@ -0,0 +1,64 @@
+package edu.emory.cs.algebraic;
+
+import java.util.Arrays;
+
+public class LongIntegerQuiz extends LongInteger {
+
+    public LongIntegerQuiz(LongInteger n) {
+        super(n);
+    }
+
+    public LongIntegerQuiz(String n) {
+        super(n);
+    }
+
+    @Override
+    public void add(LongInteger n){
+        super.add(n);
+    }
+
+    @Override
+    protected void addDifferentSign (LongInteger n) {
+        // copies largest absolute value LongInteger to result
+        // byte[] result = new byte [Math.max(digits.length, n.digits.length)];
+        // result = compareAbs(n) > 0 ? Arrays.copyOf(digits, result.length) : Arrays.copyOf(n.digits, result.length);
+
+        // sets reference result to larger LongInteger and stores sign of this in "a"
+        byte[] result = compareAbs(n) > 0 ? digits : n.digits;
+        Sign a = this.sign;
+
+        // sets reference subtract to smaller LongInteger
+        LongInteger subtract = compareAbs(n) > 0 ? n : this;
+
+        // performs the addition
+        for (int i = 0; i < subtract.digits.length; i++) {
+            if (result[i] < subtract.digits[i]) {
+                for (int j = i+1; j < result.length; j++) {
+                    if (result[j] != 0) {
+                        result[j] -= 1;
+                        result[i] += 10;
+                        break;
+                    }
+                }
+            }
+            result[i] -= subtract.digits[i];
+        }
+
+        // eliminates unnecessary elements (zeros)
+        int lengthModification = 0;
+        for (int i = result.length - 1; i >= 0; i--) {
+            if (result[i] == 0) lengthModification++;
+            else break;
+        }
+        digits = lengthModification == digits.length ? new byte[1]: Arrays.copyOf(result, result.length - lengthModification);
+
+        // sets sign
+        sign = subtract.sign == Sign.POSITIVE ? Sign.NEGATIVE : Sign.POSITIVE;
+
+        // sets sign for edge case when the result is zero
+        if (digits.length == 1 && digits[0] == 0) sign = a;
+
+        // sets sign to positive for edge case when the result is zero
+        //if (digits.length == 1 && digits[0] == 0) sign = Sign.POSITIVE;
+    }
+}
---
+++ src/main/java/edu/emory/cs/algebraic/LongIntegerRun.java
@@ -0,0 +1,25 @@
+package edu.emory.cs.algebraic;
+
+import java.util.List;
+import java.util.ArrayList;
+import java.util.Comparator;
+
+public class LongIntegerRun {
+    static public void main(String[] args) {
+        List<LongInteger> list = new ArrayList<>();
+
+        list.add(new LongInteger("78"));
+        list.add(new LongInteger("-45"));
+        list.add(new LongInteger("0"));
+        list.add(new LongInteger("6"));
+        list.add(new LongInteger("-0"));
+        list.add(new LongInteger("-123"));
+
+        list.sort(Comparator.naturalOrder());
+        System.out.println(list);
+
+        list.sort(Comparator.reverseOrder());
+        System.out.println(list);
+
+    }
+}
