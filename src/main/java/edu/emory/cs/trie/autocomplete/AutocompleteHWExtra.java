b05c17b649a548caffaa38a2245854d7f5a3e6c7
guylikekai <65786315+guylikekai@users.noreply.github.com>
2020-11-03 16:27:41 -0500

    Initial commit

--- src/main/java/edu/emory/cs/trie/autocomplete/AutocompleteHWExtra.java
+++ src/main/java/edu/emory/cs/trie/autocomplete/AutocompleteHWExtra.java
@@ -9,7 +9,6 @@ import java.io.InputStreamReader;
 import java.util.ArrayList;
 import java.util.List;
 
-
 public class AutocompleteHWExtra extends Autocomplete<List<Word>> {
     private int longest;
     public AutocompleteHWExtra(String dict_file, int max) {
@@ -149,4 +148,48 @@ public class AutocompleteHWExtra extends Autocomplete<List<Word>> {
         candidate.increment();
     }
 
+}
+
+
+
+class Word {
+    private String key;
+    private int frequency;
+
+    public Word(String key, int frequency) {
+        setKey(key);
+        setFrequency(frequency);
+    }
+
+    public void setKey(String key) {
+        this.key = key;
+    }
+
+    public String getKey() {
+        return key;
+    }
+
+    public void setFrequency(int frequency) {
+        this.frequency = frequency;
+    }
+
+    public int getFrequency() {
+        return frequency;
+    }
+
+    public static Word toWord (String word) {
+        return new Word(word, 0);
+    }
+
+    public int length() {
+        return key.length();
+    }
+
+    public void increment() {
+        frequency++;
+    }
+
+    public boolean equals(Word word) {
+        return word.getKey() == key;
+    }
 }
