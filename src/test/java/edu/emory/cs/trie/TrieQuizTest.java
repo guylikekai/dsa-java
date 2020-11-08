57d545f0c18865a3a9a5949d2dfd0f45c07bdad5
guylikekai <65786315+guylikekai@users.noreply.github.com>
2020-11-08 16:54:47 -0500

    Initial commit

--- src/test/java/edu/emory/cs/trie/TrieQuizTest.java
+++ src/test/java/edu/emory/cs/trie/TrieQuizTest.java
@@ -13,15 +13,17 @@ import static org.junit.Assert.assertEquals;
 public class TrieQuizTest {
     @Test
     public void testGetEntities() {
-        final List<String> L = List.of("United States", "South Korea", "United States Yeah");
+        final List<String> L = List.of("United States", "South Korea", "United States of America", "America");
         TrieQuiz trie = new TrieQuiz();
         for (int i = 0; i < L.size(); i++)
             trie.put(L.get(i), i);
 
-        String input = "I was born in South Korea and raised in the United States Yeah";
+        String input = "I was born in South Korea and raised in the United States of America";
         List<Entity> entities = List.of(new Entity(44, 62, 2), new Entity(44, 57, 0), new Entity(14, 25, 1));
         Set<String> expected = entities.stream().map(Entity::toString).collect(Collectors.toSet());
+        System.out.println(expected.toString());
         Set<String> actual = trie.getEntities(input).stream().map(Entity::toString).collect(Collectors.toSet());
+        System.out.println(actual.toString());
         assertEquals(expected, actual);
     }
 }
