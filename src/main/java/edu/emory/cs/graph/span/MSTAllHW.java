c3933ac2db834a8e5bd888ad0699e4e08f1970f1
guylikekai <65786315+guylikekai@users.noreply.github.com>
2020-12-07 23:03:04 -0500

    Initial commit

--- src/main/java/edu/emory/cs/graph/span/MSTAllHW.java
+++ src/main/java/edu/emory/cs/graph/span/MSTAllHW.java
@@ -14,118 +14,89 @@ public class MSTAllHW implements MSTAll {
     @Override
     public List<SpanningTree> getMinimumSpanningTrees(Graph graph) {
         PriorityQueue<Edge> queue = new PriorityQueue<>(graph.getAllEdges());
-        List<Edge> lst = new ArrayList<>(queue);
-        SpanningTree tree;
         List<SpanningTree> result = new ArrayList<>();
-        List<Edge> temp = new ArrayList<>(lst);
-        double total;
-        double max;
-        Graph graph1 = new Graph(graph);
-        Subgraph sub = new Subgraph();
         List<Edge> array = new ArrayList<>();
+        List<Integer> vertices = new ArrayList<>();
 
-        //result.add(getMST(graph, temp));
-//        total = result.get(0).getTotalWeight();
-//        max = getMaxWeight(result.get(0));
-
-//        for (Edge edge : lst) {
-//            if (edge.getWeight() > max) {
-//                graph1.getAllEdges().remove(edge);
-//            }
-//        }
-
-        int limit = graph.size();
-
-        while (limit > 0) {
-            for (int i = 0; i < graph.size(); i++) {
-                getPaths(graph, sub, result, i, limit);
-            }
-            limit--;
+        for (int i = 0; i < graph.size(); i++) {
+            vertices.add(i);
         }
 
-
-//        for (int i = 0; i < lst.size() - 1; i++) {
-//            e = lst.remove(0);
-//            lst.add(e);
-//            temp = new ArrayList<>(lst);
-//            tree = getMST(graph, temp);
-//            if (!result.contains(tree)) result.add(tree);
-//        }
+        heapPermutation(vertices, vertices.size(), graph, result);
 
 
-
-//        for (int i = 0; i < lst.size(); i++) {
-//            temp = new ArrayList<>(lst);
-//            e = temp.get(0);
-//            for (int j = i + 1; j < lst.size(); j++) {
-//                temp.set(i, temp.get(j));
-//                temp.set(j, e);
-//            }
-//        }
-
-//        for (int i = lst.size() - 1; i >= 0; i--) {
-//            e = lst.poll();
-//            lst.add(e);
-//            temp = new ArrayDeque<>(lst);
-//            tree = getMST(graph, temp);
-//            if (!result.contains(tree)) result.add(tree);
-//        }
-
-
-for (int z = 0; z < graph.size(); z++) {
-    for (int i = 0; i < result.size(); i++) {
-        for (int j = i + 1; j < result.size(); j++) {
-            array.clear();
-            array.addAll(result.get(j).getEdges());
-            System.out.println("first: " + result.get(i).getEdges());
-            System.out.println("OG: " + array);
-            for (Edge edge : result.get(i).getEdges()) {
-                for (int k = 0; k < array.size(); k++) {
-                    if (isSameEdge(edge, array.get(k))) array.remove(k);
+        for (int z = 0; z < graph.size(); z++) {
+            for (int i = 0; i < result.size(); i++) {
+                for (int j = i + 1; j < result.size(); j++) {
+                    array.clear();
+                    array.addAll(result.get(j).getEdges());
+                    for (Edge edge : result.get(i).getEdges()) {
+                        for (int k = 0; k < array.size(); k++) {
+                            if (isSameEdge(edge, array.get(k))) array.remove(k);
+                        }
+                    }
+                    if (array.isEmpty()) result.remove(j);
                 }
             }
-            System.out.println("Check: " + array);
-            if (array.isEmpty()) result.remove(j);
         }
-    }
-}
-
-
-//
-//        boolean remove = false;
-//        List<Integer> toRemove = new ArrayList<>();
-//        for (int i = 0; i < result.size(); i++) {
-//            for (int j = i +1; j < result.size(); j++) {
-//                for (Edge edge1 : result.get(i).getEdges()) {
-//                    for (Edge edge2 : result.get(j).getEdges()) {
-//                        remove = false;
-//                        if (isSameEdge(edge1, edge2)) {
-//                            remove = true;
-//                            break;
-//                        }
-//                    }
-//                    if (!remove) {
-//                        break;
-//                    }
-//                }
-//                if (remove) toRemove.add(j);
-//            }
-//        }
-//
-//
-//        List<SpanningTree> tmp = new ArrayList<>();
-//        if (!toRemove.isEmpty()) {
-//            for (int i : toRemove) {
-//                tmp.add(result.get(i));
-//            }
-//        }
-//        result.removeAll(tmp);
-//
-//        for (SpanningTree tree1 : result) {
-//            if (tree1.getTotalWeight() > total) result.remove(tree1);
-//        }
 
         return result;
+    }
+
+    private void getAll(Graph graph, Subgraph sub, List<Integer> missing, List<SpanningTree> result) {
+        if (missing.isEmpty()) {
+            //System.out.println("Done");
+            SpanningTree tree = new SpanningTree();
+            for (Edge edge : sub.getEdges()) {
+                tree.addEdge(edge);
+            }
+            if (!result.contains(tree)) result.add(tree);
+            return;
+        }
+        int target = missing.get(0);
+
+        for (Edge edge : graph.getIncomingEdges(target)) {
+            if (sub.getVertices().contains(edge.getSource())) {
+                List<Integer> temp = missing.subList(1, missing.size());
+                Subgraph temp1 = new Subgraph(sub);
+                temp1.addEdge(edge);
+                getAll(graph, temp1, temp, result);
+            } else continue;
+        }
+        return;
+
+    }
+    void heapPermutation(List<Integer> lst, int size, Graph graph, List<SpanningTree> result)
+    {
+
+        if (size == 1) {
+            Subgraph sub;
+                for (Edge edge : graph.getIncomingEdges(lst.get(0))) {
+                    sub = new Subgraph();
+                    sub.addEdge(edge);
+                    List<Integer> temp = new ArrayList<>(lst);
+                    temp.remove(temp.indexOf(edge.getSource()));
+                    temp.remove(0);
+                    getAll(graph, sub, temp, result);
+                }
+
+        }
+
+        for (int i = 0; i < size; i++) {
+            heapPermutation(lst, size - 1, graph, result);
+
+            if (size % 2 == 1) {
+                int temp = lst.get(0);
+                lst.set(0, lst.get(size-1));
+                lst.set(size - 1, temp);
+            }
+
+            else {
+                int temp = lst.get(i);
+                lst.set(i, lst.get(size - 1));
+                lst.set(size-1, temp);
+            }
+        }
     }
 
     private SpanningTree getMST(Graph graph, List<Edge> queue) {
@@ -177,7 +148,7 @@ for (int z = 0; z < graph.size(); z++) {
 
     public static void main(String args[]) {
         MSTAllHW test = new MSTAllHW();
-        Graph graph = test.getCompleteGraph(5);
+        Graph graph = test.getCompleteGraph(6);
 
 //        graph.setUndirectedEdge(0, 1, 1);
 //        graph.setUndirectedEdge(0, 2, 1);
@@ -204,74 +175,5 @@ for (int z = 0; z < graph.size(); z++) {
                 graph.setUndirectedEdge(i, j, 1);
 
         return graph;
-    }
-
-    public void getPaths(Graph graph, Subgraph sub, List<SpanningTree> result, int target, int limit) {
-        System.out.println(sub.getEdges().toString());
-        if (sub.getVertices().size() == graph.size()) {
-            System.out.println(sub.getEdges().toString());
-            count++;
-            SpanningTree tree = new SpanningTree();
-            for (Edge edge : sub.getEdges()) {
-                tree.addEdge(edge);
-            }
-            if (!result.contains(tree)) result.add(tree);
-            //System.out.println(tree.getEdges().toString());
-            return;
-        }
-        Subgraph tmp;
-        Graph tmp1;
-        for (Edge edge : graph.getIncomingEdges(target)) {
-            if (sub.contains(edge.getSource())) continue;
-            tmp = new Subgraph(sub);
-            tmp1 = new Graph(graph.size());
-
-            for (Edge edge1 : sub.getEdges()) {
-                tmp1.setDirectedEdge(edge1.getSource(), edge1.getTarget(), edge1.getWeight());
-            }
-            if (tmp1.getAllEdges().size() > 0){
-                if (check(edge.getSource(), tmp.getEdges().get(0).getTarget(), tmp1, limit)) {
-                    tmp.addEdge(edge);
-                } else {
-                    int[] max1 = new int[1];
-                    for (int vertex : tmp.getVertices()) {
-                        getLongestPath(vertex, sub.getEdges().get(0).getTarget(), tmp1, new Subgraph(), max1);
-                        int limit1 = limit - max1[0];
-                        getPaths(graph, tmp, result, vertex, limit);
-                    }
-            }
-
-            } else {
-                tmp.addEdge(edge);
-            }
-            getPaths(graph, tmp, result, edge.getSource(), limit);
-        }
-        return;
-    }
-
-    private boolean check(int target, int OGTarget, Graph sub, int limit) {
-        int[] max = new int[1];
-        max[0] = 0;
-        getLongestPath(target, OGTarget, sub, new Subgraph(), max);
-        if (max[0] > limit) return false;
-        else return true;
-    }
-
-    private void getLongestPath(int target, int OGTarget, Graph sub, Subgraph sub1, int[] max) {
-        if (target == OGTarget) {
-            if (sub1.getEdges().size() > max[0]) {
-                max[0] = sub1.getEdges().size();
-                return;
-            }
-        }
-
-        for (Edge edge : sub.getIncomingEdges(target)) {
-            if (sub1.getEdges().contains(edge)) continue;
-            Subgraph temp = new Subgraph(sub1);
-            temp.addEdge(edge);
-            getLongestPath(target, OGTarget, sub, temp, max);
-        }
-
-        return;
     }
 }
